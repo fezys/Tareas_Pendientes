18b3399220695becdd7cfe45a0925d96d4126a1f
fezys <1008120192@unajma.edu.pe>
2021-12-19 11:18:28 -0500

    Conexión

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php 
+/*los datos aqui se tiene que cambiar de acuero 
+a los datos dados por el host al cual se subió el proyecto*/
+    // $hn = 'localhost';
+    // $db = 'base_de_datos';
+    // $un = 'usuario';
+    // $pw = 'password';
+    
+    $hn = 'localhost';
+    $db = 'pendientes';
+    $un = 'root';
+    $pw = '';
+    $port = 3360;
+
+?>
+
+
+
