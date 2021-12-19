b8d5d79957456ec1daf9fe5cb3604a0871649436
fezys <1008120192@unajma.edu.pe>
2021-12-19 11:19:46 -0500

    Cerrar sessión

---
+++ cerrar.php
@@ -0,0 +1,25 @@
+<?php //logout.php
+//cerrar sesión
+    session_start();
+
+    if (isset($_SESSION['nombre']))
+    {
+        $nombre = $_SESSION['nombre'];
+        $apellido = $_SESSION['apellido'];
+
+        destroy_session_and_data();
+
+        header('Location: index.php');
+        die();
+    }
+    else echo "Por favor <a href='index.php'>Click aqui</a>
+                para iniciar sesion";
+
+    function destroy_session_and_data()
+    {
+        //session_start();
+        $_SESSION = array();
+        setcookie(session_name(), '', time()-2592000, '/');
+        session_destroy();
+    }
+?>
